0b76274e4e63815540293731f3fedadf8ea2efa4
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-10-31 15:14:06 +0900

    first commit

---
+++ src/main/java/ipeko/tonbanjan/TonbanjanApplication.java
@@ -0,0 +1,13 @@
+package ipeko.tonbanjan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonbanjanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonbanjanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipeko/tonbanjan/TonbanjanApplicationTests.java
@@ -0,0 +1,13 @@
+package ipeko.tonbanjan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonbanjanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
