627d27fd37c3a6e4b585acb7cbcdb9f437151512
e1b21009 <e1b21009@oit.ac.jp>
2023-11-10 12:55:44 +0900

    class.htmlを返す処理を追加

--- src/main/java/ipeko/tonbanjan/controller/TonbanjanController.java
+++ src/main/java/ipeko/tonbanjan/controller/TonbanjanController.java
@@ -1,5 +1,8 @@
 package ipeko.tonbanjan.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ipeko.tonbanjan.model.Questions;
+import ipeko.tonbanjan.model.QuestionsMapper;
+
 @Controller
 public class TonbanjanController {
+
+  @Autowired
+  QuestionsMapper qMapper;
 
   /**
    * sample21というGETリクエストがあったら sample21()を呼び出し，sample21.htmlを返す
@@ -16,6 +25,13 @@ public class TonbanjanController {
   @GetMapping("/waitingRoom")
   public String sample21() {
     return "waitroom.html";
+  }
+
+  @GetMapping("/class")
+  public String showClass(ModelMap model) {
+    ArrayList<Questions> questions = qMapper.selectAllQuestions();
+    model.addAttribute("questions", questions);
+    return "class.html";
   }
 
 }
