c0183a87fbb2dedcb5141a4997b760af2ca73c45
e1b21016 <e1b21016@oit.ac.jp>
2023-11-14 16:44:13 +0900

    DBに選択肢の数を追加

---
+++ .log/.history/tonbanjan/src/main/resources/data_20231114163805.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+  answers (questionId, a_content)
+VALUES
+  (1, '回答1');
+
+INSERT INTO
+  questions (roomId, q_content)
+VALUES
+  (1, '質問1');
+
+INSERT INTO
+  users (userName, password)
+VALUES
+  ('user1', 'password1');
+
+INSERT INTO
+  attendance (roomId, userId, attend_times)
+VALUES
+  (1, 1, 1);
+
