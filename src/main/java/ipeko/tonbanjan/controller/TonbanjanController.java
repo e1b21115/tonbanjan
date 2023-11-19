7d9b95d7d8beb6cd5085186be73b784fd6c8c908
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-11-19 19:04:31 +0900

    解答フォームの追加

--- src/main/java/ipeko/tonbanjan/controller/TonbanjanController.java
+++ src/main/java/ipeko/tonbanjan/controller/TonbanjanController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import ipeko.tonbanjan.model.AnswersQuestions;
+import ipeko.tonbanjan.model.Answers;
+import ipeko.tonbanjan.model.AnswersMapper;
 import ipeko.tonbanjan.model.Questions;
 import ipeko.tonbanjan.model.QuestionsMapper;
 
@@ -19,6 +22,9 @@ public class TonbanjanController {
 
   @Autowired
   QuestionsMapper qMapper;
+
+  @Autowired
+  AnswersMapper aMapper;
 
   /**
    * sample21というGETリクエストがあったら sample21()を呼び出し，sample21.htmlを返す
@@ -36,13 +42,74 @@ public class TonbanjanController {
   }
 
   @PostMapping("/addQuestion")
-  public String addQuestion(@RequestParam String q_content,@RequestParam Integer q_amount, ModelMap model, Principal prin){
+  public String addQuestion(@RequestParam String q_content, @RequestParam String a_content1,
+      @RequestParam String a_content2,
+      @RequestParam String a_content3, @RequestParam String a_content4, ModelMap model,
+      Principal prin) {
     Questions que = new Questions();
     que.setQ_content(q_content);
     que.setRoomId(2);
-    que.setQuestionAmount(q_amount);
+    // que.setQuestionAmount(q_amount);
+
+    // model.addAttribute("q_amount", q_amount);
     qMapper.insertQuestions(que);
+    int questionId = que.getQuestionId();
+
+    if (!a_content1.equals("")) {
+      Answers ans1 = new Answers();
+      ans1.setA_content(a_content1);
+      ans1.setQuestionId(questionId);
+      aMapper.insertAnswers(ans1);
+    }
+    if (!a_content2.equals("")) {
+      Answers ans2 = new Answers();
+      ans2.setA_content(a_content2);
+      ans2.setQuestionId(questionId);
+      aMapper.insertAnswers(ans2);
+    }
+    if (!a_content3.equals("")) {
+      Answers ans3 = new Answers();
+      ans3.setA_content(a_content3);
+      ans3.setQuestionId(questionId);
+      aMapper.insertAnswers(ans3);
+    }
+    if (!a_content4.equals("")) {
+      Answers ans4 = new Answers();
+      ans4.setA_content(a_content4);
+      ans4.setQuestionId(questionId);
+      aMapper.insertAnswers(ans4);
+    }
     return "class.html";
   }
 
+  // @PostMapping("/addAnswers")
+  // public String addAnswer(ModelMap model,
+  // Principal prin) {
+  // int questionId = qMapper;
+  // if (a_content1 != null) {
+  // Answers ans1 = new Answers();
+  // ans1.setA_content(a_content1);
+  // ans1.setQuestionId(questionId);
+  // aMapper.insertAnswers(ans1);
+  // }
+  // if (a_content2 != null) {
+  // Answers ans2 = new Answers();
+  // ans2.setA_content(a_content2);
+  // ans2.setQuestionId(questionId);
+  // aMapper.insertAnswers(ans2);
+  // }
+  // if (a_content3 != null) {
+  // Answers ans3 = new Answers();
+  // ans3.setA_content(a_content3);
+  // ans3.setQuestionId(questionId);
+  // aMapper.insertAnswers(ans3);
+  // }
+  // if (a_content4 != null) {
+  // Answers ans4 = new Answers();
+  // ans4.setA_content(a_content4);
+  // ans4.setQuestionId(questionId);
+  // aMapper.insertAnswers(ans4);
+  // }
+  // return "class.html";
+  // }
 }
