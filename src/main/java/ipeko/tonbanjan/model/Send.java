35e4185a9a1ed46e912d81ec44a45f822c6d4fe6
e1b21062 <e1b21062@st.oit.ac.jp>
2023-11-26 12:51:16 +0900

    class.htmlに選択肢ごとの回答数を表示

--- src/main/java/ipeko/tonbanjan/model/Send.java
+++ src/main/java/ipeko/tonbanjan/model/Send.java
@@ -2,18 +2,19 @@ package ipeko.tonbanjan.model;
 
 public class Send {
     public int answerId, questionId, sAnswerId;
+    
     public int getsendId() {
         return answerId;
       }
-    
+
       public void setsendId(int answerId) {
         this.answerId = answerId;
       }
-    
+
       public int getQuestionId() {
         return questionId;
       }
-    
+
       public void setQuestionId(int questionId) {
         this.questionId = questionId;
       }
