3b71432e02c140058cdbbac78fd384103ee73e3a
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-12-22 10:38:51 +0900

    認証・認可処理をazure adに変更

--- src/main/java/ipeko/tonbanjan/security/TonbanjanAuthConfiguration.java
+++ src/main/java/ipeko/tonbanjan/security/TonbanjanAuthConfiguration.java
@@ -1,18 +1,53 @@
 package ipeko.tonbanjan.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class TonbanjanAuthConfiguration {
+  @Value("${azure.ad.tenant-id}")
+  private String tenantId;
+
+  @Value("${azure.ad.client-id}")
+  private String clientId;
+
+  @Value("${azure.ad.client-secret}")
+  private String clientSecret;
+
+  @Value("${azure.ad.redirect-uri}")
+  private String redirectUri;
+
+  @Value("${azure.ad.logout-uri}")
+  private String logoutUri;
+
+  @Value("${azure.ad.authorization-uri}")
+  private String authorization;
+
+  @Value("${azure.ad.token-uri}")
+  private String token;
+
+  @Value("${azure.ad.user-info-uri}")
+  private String userInfo;
+
+  @Value("${azure.ad.jwk-set-uri}")
+  private String jwkSetUri;
+
   /**
    * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
    *
@@ -22,16 +57,15 @@ public class TonbanjanAuthConfiguration {
    */
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.formLogin(login -> login
-        .permitAll())
-        .logout(logout -> logout
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
+    http
         .authorizeHttpRequests(authz -> authz
             .requestMatchers(AntPathRequestMatcher.antMatcher("/waitingRoom/**"))
             .authenticated()
             .requestMatchers(AntPathRequestMatcher.antMatcher("/**"))
             .permitAll())// 上記以外は全員アクセス可能
+        .logout(logout -> logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
         .csrf(csrf -> csrf
             .ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/*")))// h2-console用にCSRF対策を無効化
         .headers(headers -> headers
@@ -46,24 +80,24 @@ public class TonbanjanAuthConfiguration {
    * @return
    */
   @Bean
-  public InMemoryUserDetailsManager userDetailsService() {
-
-    // ユーザ名，パスワード，ロールを指定してbuildする
-    // このときパスワードはBCryptでハッシュ化されているため，{bcrypt}とつける
-    // ハッシュ化せずに平文でパスワードを指定する場合は{noop}をつける
-    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
-    // $ sshrun htpasswd -nbBC 10 user1 p@ss
-
-    // $ sshrun htpasswd -nbBC 10 user1 p@ss
-    UserDetails user1 = User.withUsername("user1")
-        .password("{bcrypt}$2y$10$RTBVuLU8cWogbgMrPoPItuPz.kZSNG8q.98jVKNJBFqx6v0G8UdQu").roles("USER").build();
-    UserDetails user2 = User.withUsername("user2")
-        .password("{bcrypt}$2y$10$EHxZV90jvMPpdG93R39fOutiBp103qRsUSMswcY62mOVRTpPpBqE.").roles("USER").build();
-    UserDetails admin = User.withUsername("admin")
-        .password("{bcrypt}$2y$10$dNYDu/FsSxtRYLwDu/coKOQKXH8EmyRTnyj/teQjItpJzGJTnEADu").roles("ADMIN").build();
-
-    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
-    return new InMemoryUserDetailsManager(user1, user2, admin);
+  public ClientRegistrationRepository clientRegistrationRepository() {
+    return new InMemoryClientRegistrationRepository(clientRegistration());
   }
 
+  private ClientRegistration clientRegistration() {
+    return ClientRegistration.withRegistrationId("example-client")
+        .clientId("your-client-id")
+        .clientSecret("your-client-secret")
+        .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+        .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+        .redirectUri("your-redirect-uri")
+        .scope("openid", "profile", "email")
+        .authorizationUri("authorization-uri")
+        .tokenUri("token-uri")
+        .userInfoUri("user-info-uri")
+        .userNameAttributeName(IdTokenClaimNames.SUB)
+        .jwkSetUri("jwk-set-uri")
+        .clientName("東盤雀")
+        .build();
+  }
 }
